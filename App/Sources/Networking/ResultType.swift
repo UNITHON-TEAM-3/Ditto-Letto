5d06abe0436b672d5551c9218edbeef2e6075155
Hyeonjoonn <phj3046@gmail.com>
2023-09-24 02:58:55 +0900

    fix: NetworkingResult Code 삭제된 case 복구

--- App/Sources/Networking/ResultType.swift
+++ App/Sources/Networking/ResultType.swift
@@ -2,8 +2,11 @@ import Foundation
 
 enum NetworkingResult: Int {
     case getOk = 200
+    case deleteOk = 204
+    case createOk = 201
     case wrongRq = 400
-    case payloadTooLarge = 413
-    case unsupportedMediaType = 415
+    case tokenError = 401
+    case notFound = 404
+    case conflict = 409
     case fault = 0
 }
