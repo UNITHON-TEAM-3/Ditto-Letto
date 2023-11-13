203ecda585a6aa2d406cd4d81d773b357567be6b
InhyeKang <kanginhae0826@gmail.com>
2023-11-12 22:00:42 -0800

    feat :: OnboardingView SwiftUI -> UIKit

--- App/Sources/Scene/Onboarding/OnboardingViewer.swift
+++ App/Sources/Scene/Onboarding/OnboardingViewer.swift
@@ -1,34 +1,55 @@
-import SwiftUI
+import UIKit
 import DesignSystem
 
-struct OnboardingImageViewer: View {
-    private let onboardingViews: [(image: Image, color: Color)] = [
-        (Image(uiImage: .Image.first), .clear),
-        (Image(uiImage: .Image.second), .clear),
-        (Image(uiImage: .Image.third), .clear),
-        (Image(uiImage: .Image.fourth), .clear)
+class OnboardingImageViewer: UIViewController, UIScrollViewDelegate {
+    private let onboardingViews: [(image: UIImage, color: UIColor)] = [
+        (.Image.first, .clear),
+        (.Image.second, .clear),
+        (.Image.third, .clear),
+        (.Image.fourth, .clear)
     ]
 
-    init() {
-        UIPageControl.appearance().setIndicatorImage(.Image.pinkHeart, forPage: UIPageControl.appearance().currentPage)
+    let scrollView = UIScrollView()
+    let pageControl = UIPageControl()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .color(.dittoLettoColor(.main))
+        setupScrollView()
+        setupPageControl()
     }
 
-    var body: some View {
-        TabView {
-            ForEach(self.onboardingViews.indices, id: \.self) { index in
-                VStack {
-                    ZStack(alignment: .center) {
-                        onboardingViews[index].image
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(height: UIScreen.main.bounds.height * 0.811)
-                            .padding(.top, UIScreen.main.bounds.height * 0.137)
-                    }
-                }
-            }
+    func setupScrollView() {
+        scrollView.frame = self.view.bounds
+        scrollView.delegate = self
+        scrollView.isPagingEnabled = true
+        scrollView.showsHorizontalScrollIndicator = false
+        self.view.addSubview(scrollView)
+
+        for idx in 0..<onboardingViews.count {
+            let imageView = UIImageView()
+            imageView.image = onboardingViews[idx].image
+            imageView.contentMode = .scaleAspectFit
+            imageView.frame = CGRect(x: UIScreen.main.bounds.width * CGFloat(idx),
+                                     y: UIScreen.main.bounds.height * 0.137,
+                                     width: UIScreen.main.bounds.width,
+                                     height: UIScreen.main.bounds.height * 0.811)
+            scrollView.addSubview(imageView)
         }
-        .tabViewStyle(PageTabViewStyle())
-        .edgesIgnoringSafeArea(.top)
+
+        scrollView.contentSize = CGSize(width: UIScreen.main.bounds.width * CGFloat(onboardingViews.count), height: scrollView.frame.height)
     }
 
+    func setupPageControl() {
+        pageControl.numberOfPages = onboardingViews.count
+        pageControl.currentPageIndicatorTintColor = .red // 현재 페이지를 나타내는 표시 색상을 변경합니다. 적절한 색상을 설정해주세요.
+        pageControl.pageIndicatorTintColor = .gray // 다른 페이지를 나타내는 표시 색상을 변경합니다. 적절한 색상을 설정해주세요.
+        pageControl.center = self.view.center
+        self.view.addSubview(pageControl)
+    }
+
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let pageNumber = round(scrollView.contentOffset.x / scrollView.frame.size.width)
+        pageControl.currentPage = Int(pageNumber)
+    }
 }
