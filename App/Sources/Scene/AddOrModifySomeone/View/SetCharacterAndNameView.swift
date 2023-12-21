e74a28dfaab3532936e8b4a210232dbb263165c8
Hyeonjoonn <phj3046@gmail.com>
2023-12-21 14:47:35 +0900

    feat :: 전화번호부 추가 및 수정 화면 캐릭터 설정 & 이름 설정 UI

--- App/Sources/Scene/AddOrModifySomeone/View/SetCharacterAndNameView.swift
+++ App/Sources/Scene/AddOrModifySomeone/View/SetCharacterAndNameView.swift
@@ -1,6 +1,8 @@
 import UIKit
 import DesignSystem
 import SnapKit
+import RxSwift
+import RxCocoa
 
 final class SetCharacterAndNameView: UIView {
     // MARK: - Properties
@@ -17,15 +19,52 @@ final class SetCharacterAndNameView: UIView {
     private let bodyView: UIView = {
         $0.backgroundColor = .color(.dittoLettoColor(.gray1))
         $0.layer.borderColor = UIColor.black.cgColor
-        $0.layer.borderWidth = 0.8
+        $0.layer.borderWidth = 1
         return $0
     }(UIView())
+    private let setCharacterLabel: UILabel = {
+        $0.text = "캐릭터 설정"
+        $0.font = .ramche(.subheadline)
+        return $0
+    }(UILabel())
+    private let setCharacterBackgroundView: UIView = {
+        $0.backgroundColor = UIColor.white
+        $0.layer.borderColor = UIColor.systemGray3.cgColor //color(.dittoLettoColor(.gray2)).cgColor
+        $0.layer.borderWidth = 2
+        return $0
+    }(UIView())
+    private lazy var setCharacterStackView: UIStackView = {
+        $0.axis = .horizontal
+        $0.distribution = .fillEqually
+        $0.spacing = 15
+        $0.backgroundColor = .clear
+        return $0
+    }(UIStackView())
+    private let setNameLabel: UILabel = {
+        $0.text = "이름 설정"
+        $0.font = .ramche(.subheadline)
+        return $0
+    }(UILabel())
+    private let setNameTextFieldBackgroundView: UIView = {
+        $0.backgroundColor = UIColor.white
+        $0.layer.borderColor = UIColor.systemGray3.cgColor //color(.dittoLettoColor(.gray2)).cgColor
+        $0.layer.borderWidth = 2
+        return $0
+    }(UIView())
+    let nameTextField: UITextField = {
+        $0.font = .ramche(.subheadline)
+        $0.textColor = .black
+        return $0
+    }(UITextField())
+    var selectedButton: UIButton?
+    var disposeBag = DisposeBag()
     // MARK: - Life Cycles
     init() {
         super.init(frame: .zero)
         backgroundColor = .clear
         addView()
         setLayout()
+        bind()
     }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -38,6 +77,16 @@ final class SetCharacterAndNameView: UIView {
             addSubview($0)
         }
         topBarView.addSubview(topBarDetailView)
+        [
+            setCharacterLabel,
+            setCharacterBackgroundView,
+            setNameLabel,
+            setNameTextFieldBackgroundView
+        ].forEach {
+            bodyView.addSubview($0)
+        }
+        setCharacterBackgroundView.addSubview(setCharacterStackView)
+        setNameTextFieldBackgroundView.addSubview(nameTextField)
     }
     private func setLayout() {
         topBarView.snp.makeConstraints { make in
@@ -53,6 +102,68 @@ final class SetCharacterAndNameView: UIView {
         bodyView.snp.makeConstraints { make in
             make.top.equalTo(topBarView.snp.bottom)
             make.leading.trailing.bottom.equalToSuperview()
+        }
+        setCharacterLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(UIScreen.main.bounds.height * 0.02)
+            make.leading.equalToSuperview().inset(UIScreen.main.bounds.width * 0.035)
+        }
+        setCharacterBackgroundView.snp.makeConstraints { make in
+            make.top.equalTo(setCharacterLabel.snp.bottom).inset(-(UIScreen.main.bounds.height * 0.02))
+            make.leading.trailing.equalToSuperview().inset(UIScreen.main.bounds.width * 0.03)
+            make.height.equalToSuperview().multipliedBy(0.3)
+        }
+        setCharacterStackView.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.trailing.equalToSuperview().inset(UIScreen.main.bounds.width * 0.035)
+            make.height.equalToSuperview().multipliedBy(0.7)
+        }
+        setNameLabel.snp.makeConstraints { make in
+            make.top.equalTo(setCharacterBackgroundView.snp.bottom).inset(-(UIScreen.main.bounds.height * 0.03))
+            make.leading.equalToSuperview().inset(UIScreen.main.bounds.width * 0.035)
+        }
+        setNameTextFieldBackgroundView.snp.makeConstraints { make in
+            make.top.equalTo(setNameLabel.snp.bottom).inset(-(UIScreen.main.bounds.height * 0.02))
+            make.leading.trailing.equalToSuperview().inset(UIScreen.main.bounds.width * 0.03)
+            make.height.equalToSuperview().multipliedBy(0.17)
+        }
+        nameTextField.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview()
+            make.leading.trailing.equalToSuperview().inset(UIScreen.main.bounds.width * 0.03)
+        }
+    }
+    private func bind() {
+        CharacterType.allCases.forEach { type in
+            let button: UIButton = {
+                let btn = UIButton()
+                switch type {
+                case .normal:
+                    btn.setImage(.Image.normalIcon, for: .normal)
+                    btn.setImage(.Image.selectedNormalIcon, for: .selected)
+                case .bad:
+                    btn.setImage(.Image.badIcon, for: .normal)
+                    btn.setImage(.Image.selectedBadIcon, for: .selected)
+                case .good:
+                    btn.setImage(.Image.goodIcon, for: .normal)
+                    btn.setImage(.Image.selectedGoodIcon, for: .selected)
+                case .sad:
+                    btn.setImage(.Image.sadIcon, for: .normal)
+                    btn.setImage(.Image.selectedSadIcon, for: .selected)
+                }
+                return btn
+            }()
+            button.rx.tap
+                .subscribe(onNext: { [weak self] in
+                    self?.selectedButton?.isSelected = false
+                    self?.selectedButton = button
+                    button.isSelected.toggle()
+                })
+                .disposed(by: disposeBag)
+            setCharacterStackView.addArrangedSubview(button)
+            // 초기 selected 값
+            if type == .normal {
+                button.isSelected = true
+                selectedButton = button
+            }
         }
     }
 }
