24bfc9f6e71220d4e17ccfa438d0f4073fb34b1b
InhyeKang <kanginhae0826@gmail.com>
2023-12-27 23:51:10 +0900

    ♻️ :: 일단 bar 추가
     bar center를 어디에 맞춰야 할까

--- DesignSystem/Sources/BackgroundView/MyPageContentView.swift
+++ DesignSystem/Sources/BackgroundView/MyPageContentView.swift
@@ -13,11 +13,39 @@ public final class MyPageContentView: UIView {
         $0.backgroundColor = .color(.dittoLettoColor(.dark))
         return $0
     }(UIView())
+    private let contentView: UIView = {
+        $0.backgroundColor = .color(.dittoLettoColor(.gray1))
+        return $0
+    }(UIView())
     private lazy var contentStackView = VStackView(spacing: 0) {
         ZStackView {
             headerView
             leftBar
             rightBar
         }
+        contentView
+    }
+
+    init(
+        leftBarWidth: CGFloat,
+        leftBarHeight: CGFloat,
+        rightBarWidth: CGFloat,
+        rightBarHeight: CGFloat
+    ) {
+        super.init(frame: .zero)
+
+        leftBar.translatesAutoresizingMaskIntoConstraints = false
+        rightBar.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            leftBar.widthAnchor.constraint(equalToConstant: leftBarWidth),
+            leftBar.heightAnchor.constraint(equalToConstant: leftBarHeight),
+            rightBar.widthAnchor.constraint(equalToConstant: rightBarWidth),
+            rightBar.heightAnchor.constraint(equalToConstant: rightBarHeight)
+        ])
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
