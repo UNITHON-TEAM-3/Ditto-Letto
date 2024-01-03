7d85fd6a0f99c017bc3d0dc6dd17ea436c4fd956
InhyeKang <kanginhae0826@gmail.com>
2024-01-03 23:52:35 +0900

    refactor :: MyPageViewType withdrawal 추가

--- DesignSystem/Sources/BackgroundView/MyPageContentView.swift
+++ DesignSystem/Sources/BackgroundView/MyPageContentView.swift
@@ -2,8 +2,9 @@ import UIKit
 
 public enum MyPageViewType: String {
     case table
-    case notice
     case auth
+    case withdrawal
+    case notice
 }
 
 public final class MyPageContentView: UIView {
@@ -43,10 +44,12 @@ public final class MyPageContentView: UIView {
         switch type {
         case .table:
             setTableType()
-        case .notice:
-            setNoticeType()
         case .auth:
             setAuthType()
+        case .withdrawal:
+            setWithdrawalType()
+        case .notice:
+            setNoticeType()
         }
     }
 
@@ -73,6 +76,23 @@ extension MyPageContentView {
             leftBar.rightAnchor.constraint(equalTo: rightBar.leftAnchor, constant: 8)
         ])
     }
+    func setAuthType() {
+        headerView.backgroundColor = .color(.dittoLettoColor(.gray1))
+        contentView.backgroundColor = .color(.dittoLettoColor(.white))
+        leftBar.backgroundColor = .color(.dittoLettoColor(.gray2))
+        rightBar.backgroundColor = .color(.dittoLettoColor(.gray2))
+        NSLayoutConstraint.activate([
+            headerView.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height * 0.064),
+            rightBar.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width * 0.061),
+            rightBar.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height * 0.024),
+            rightBar.centerYAnchor.constraint(equalTo: headerView.centerYAnchor),
+            rightBar.rightAnchor.constraint(equalTo: headerView.rightAnchor, constant: UIScreen.main.bounds.width * 0.053),
+            leftBar.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width * 0.061),
+            leftBar.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height * 0.01),
+            leftBar.centerYAnchor.constraint(equalTo: headerView.centerYAnchor),
+            leftBar.rightAnchor.constraint(equalTo: rightBar.leftAnchor, constant: UIScreen.main.bounds.width * 0.042)
+        ])
+    }
     func setNoticeType() {
         headerView.backgroundColor = .color(.dittoLettoColor(.third))
         contentView.backgroundColor = .color(.dittoLettoColor(.dark))
@@ -90,21 +110,21 @@ extension MyPageContentView {
             leftBar.rightAnchor.constraint(equalTo: rightBar.leftAnchor, constant: UIScreen.main.bounds.width * 0.042)
         ])
     }
-    func setAuthType() {
+    func setWithdrawalType() {
         headerView.backgroundColor = .color(.dittoLettoColor(.gray1))
         contentView.backgroundColor = .color(.dittoLettoColor(.white))
         leftBar.backgroundColor = .color(.dittoLettoColor(.gray2))
         rightBar.backgroundColor = .color(.dittoLettoColor(.gray2))
         NSLayoutConstraint.activate([
-            headerView.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height * 0.064),
-            rightBar.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width * 0.061),
-            rightBar.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height * 0.024),
+            headerView.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height * 0.03),
+            rightBar.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width * 0.037),
+            rightBar.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height * 0.007),
             rightBar.centerYAnchor.constraint(equalTo: headerView.centerYAnchor),
-            rightBar.rightAnchor.constraint(equalTo: headerView.rightAnchor, constant: UIScreen.main.bounds.width * 0.053),
-            leftBar.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width * 0.061),
-            leftBar.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height * 0.01),
+            rightBar.rightAnchor.constraint(equalTo: headerView.rightAnchor, constant: UIScreen.main.bounds.width * 0.027),
+            leftBar.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width * 0.037),
+            leftBar.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height * 0.012),
             leftBar.centerYAnchor.constraint(equalTo: headerView.centerYAnchor),
-            leftBar.rightAnchor.constraint(equalTo: rightBar.leftAnchor, constant: UIScreen.main.bounds.width * 0.042)
+            leftBar.rightAnchor.constraint(equalTo: rightBar.leftAnchor, constant: UIScreen.main.bounds.width * 0.016)
         ])
     }
 }
